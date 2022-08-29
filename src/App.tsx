ab68ff232ad6bd2daa20589e0c6562af888392ff
sayuri.hamada.zizo <hamada@zizo.ne.jp>
2022-08-29 18:16:20 +0900

    UI作成途中

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='l-psge'>
+      <BrowserRouter>
+        <header className='l-header'>
+          <div className='l-header__inner'>
+            <h1 className='l-header-ttl'>TODO LIST</h1>
+            <button className='l-header-add-btn'>＋</button>
+            <p className='l-header-link'><Link to='/complete-list'>Complete List</Link></p>
+          </div>
+        </header>
+
+        <div className='todo-list'>
+          <h2>未完了のタスク</h2>
+          <div className='todo-list-card'>
+            <div>
+              <p>タスク名</p>
+              <p>完了日：2022/09/01</p>
+            </div>
+            <div>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </div>
+        </div>
+
+
+
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
